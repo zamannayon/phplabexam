5c01bb5e28222fbaf37f1ec80ab9450dd074a9ba
zamannayon <136717578+zamannayon@users.noreply.github.com>
2023-06-21 16:16:31 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	include "dbcont.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>	
+	
+		<center>
+
+			<h1>Welcome To PHP Lab</h1>
+		  <pre><h2>Don't have an account?  <a href="registration.php"><button style="background-color:#0c3294;border: none;padding: 15px;border-radius: 10px;color:white;">Registration </button></a></h2></pre><br>
+		   <pre><h2>Already have an account?  <a href="login.php"><button style="background-color:#0c3294;border: none;padding: 15px;border-radius: 10px;color:white;">Sign In</button></a></h2></pre>
+		</center>
+	
+</body>
+</html>
+
+
+
+
